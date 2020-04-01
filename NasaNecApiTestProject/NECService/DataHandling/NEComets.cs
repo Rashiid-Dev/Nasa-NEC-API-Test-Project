0dac3f1861822a92b65e79918e77b0289350ba21
Abdirashiid Jama <rashiid.py@gmail.com>
2020-04-01 12:08:12 +0100

    Created NEComets.cs and NECometsModel in the DataHandling folder & placeholder README.md

---
+++ NasaNecApiTestProject/NECService/DataHandling/NEComets.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace NasaNecApiTestProject.NECService.DataHandling
+{
+    class NEComets
+    { 
+
+
+        public NECometsModel deser { get; set; }
+
+        public void DeserializeInfo(string DeserResponse)
+        {
+            deser = JsonConvert.DeserializeObject<NECometsModel>(DeserResponse);
+        }
+    }
+}
---
+++ NasaNecApiTestProject/NECService/DataHandling/NECometsModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NasaNecApiTestProject.NECService.DataHandling
+{
+    class NECometsModel
+    {
+
+        public class Rootobject
+        {
+            public string _object { get; set; }
+            public string epoch_tdb { get; set; }
+            public string tp_tdb { get; set; }
+            public string e { get; set; }
+            public string i_deg { get; set; }
+            public string w_deg { get; set; }
+            public string node_deg { get; set; }
+            public string q_au_1 { get; set; }
+            public string q_au_2 { get; set; }
+            public string p_yr { get; set; }
+            public string moid_au { get; set; }
+            public string _ref { get; set; }
+            public string object_name { get; set; }
+        }
+
+    }
+}
